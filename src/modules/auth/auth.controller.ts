341a574b0f2e3b498d6c284af89583dc440c9984
mohdbinharun56 <arahaman461@gmail.com>
2026-05-22 01:02:46 +0600

    [Auth]-> implement the login procedure

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,17 +7,32 @@ const signup = async (req: Request, res: Response) =>{
         const signupData = req.body;
 
         const result = await authService.signupIntoDB(signupData);
+
         if(result.rows.length === 0){
-            return SendResponse(res,401,false,"Unathorized!!");
+            return SendResponse(res,401,false,"User does not registered successfully!!");
         }
+
         delete result.rows[0].password;
+
         return SendResponse(res,200,true,"User registered successfully", result.rows[0]); 
     } catch (error: any) {
        return SendResponse(res,500,false,error.message,error);
     }
 }
 
+const login = async (req: Request, res: Response)=>{
+    try {
+        const loginData = req.body;
+
+        const result = await authService.loginIntoDB(loginData);
+
+        return SendResponse(res,200,true,"Login successful",result);
+
+    } catch (error: any) {
+        return SendResponse(res,500,false,error.message,error);
+    }
+}
 export const authController = {
  signup,
-
+ login
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -13,7 +13,36 @@ const signupIntoDB = async (signupData: IAuthSignup) => {
     
     return result;
 }
+
+
+const loginIntoDB = async (loginData: any)=>{
+    // console.log(loginData);
+    const {email, password} = loginData;
+
+    const isUserExist = await pool.query(`
+    SELECT * FROM users WHERE email = $1
+    `,[email]);
+
+    // console.log(isUserExist);
+    if(isUserExist.rows.length === 0){
+        throw new Error("Invalid credentials!");
+    }
+
+    const user = isUserExist.rows[0];
+
+    const matchPassword = bcrypt.compare(password,user.password);
+
+    if(!matchPassword){
+        throw new Error("Invalid credentials!");
+    }
+
+    delete user.password;
+
+    return {user};
+}
+
 export const authService = {
     signupIntoDB,
+    loginIntoDB
 }
 
