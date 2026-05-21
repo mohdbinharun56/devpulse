00b5c9335022a4374c49c1d008c4e63ba59d0d3d
mohdbinharun56 <arahaman461@gmail.com>
2026-05-22 00:45:20 +0600

    [HasH-Password] install bcrypt and hash the registered password

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,12 +1,15 @@
 import { pool } from "../../database/index.db";
 import type { IAuthSignup } from "./auth.interface";
+import bcrypt from "bcrypt";
 
-const signupIntoDB = (signupData: IAuthSignup) => {
+const signupIntoDB = async (signupData: IAuthSignup) => {
     const {name,email,password,role} =signupData;
+
+    const hashPassword = await bcrypt.hash(password, 12);
 
     const result = pool.query(`
     INSERT INTO users(name,email,password,role) VALUES ($1,$2,$3,COALESCE($4, 'contributor'))
-    RETURNING *`,[name,email,password,role]);
+    RETURNING *`,[name,email,hashPassword,role]);
     
     return result;
 }
